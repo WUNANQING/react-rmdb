46124e0ccaf595d5a41ac5a3993040ac6df925dc
BenWU <a1993112@gmail.com>
2021-07-17 13:37:40 +0800

    update: 修改成class component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,5 @@
 import { IMAGE_BASE_URL, POSTER_SIZE } from "../config";
-// router
+// router ver6 的useParams 不支援class component
 import { useParams } from "react-router-dom";
 // components
 import BreadCrumb from "./BreadCrumb";
@@ -11,26 +11,92 @@ import Actor from "./Actor";
 // img
 import NoImage from "../images/no_image.jpg"
 // Hook
-import useMovieFetch from "../hooks/useMovieFetch";
+// import useMovieFetch from "../hooks/useMovieFetch";
 
-const Movie = () => {
-    const { movieId } = useParams()
-    const { state: movie, loading, error } = useMovieFetch(movieId)
+// const Movie = () => {
+//     const { movieId } = useParams()
+//     const { state: movie, loading, error } = useMovieFetch(movieId)
 
-    if (loading) return <Spinner />
-    if (error) return <div>Something went wrong...</div>
-    return (
-        <>
-            <BreadCrumb movieTitle={movie.original_title} />
-            <MovieInfo movie={movie} />
-            <MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue} />
-            <Grid header="Actors">
-                {movie.actors.map(actor => (
-                    <Actor key={actor.credit_id} name={actor.name} character={actor.character} imageUrl={actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}` : NoImage} />
-                ))}
-            </Grid>
-        </>
-    )
+//     if (loading) return <Spinner />
+//     if (error) return <div>Something went wrong...</div>
+//     return (
+//         <>
+//             <BreadCrumb movieTitle={movie.original_title} />
+//             <MovieInfo movie={movie} />
+//             <MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue} />
+//             <Grid header="Actors">
+//                 {movie.actors.map(actor => (
+//                     <Actor key={actor.credit_id} name={actor.name} character={actor.character} imageUrl={actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}` : NoImage} />
+//                 ))}
+//             </Grid>
+//         </>
+//     )
+// }
+
+// export default Movie;
+
+import API from "../API";
+import { Component } from "react";
+
+class Movie extends Component {
+    state = {
+        movie: {},
+        loading: true,
+        error: false
+    }
+
+    fetchMovie = async () => {
+        const { movieId } = this.props.params
+        try {
+            this.setState({loading:true, error: false})
+
+            const movie = await API.fetchMovie(movieId);
+            const credits = await API.fetchCredits(movieId);
+            // get directors only
+            const directors = credits.crew.filter(
+                member => member.job === 'Director'
+            )
+            this.setState({
+                movie: {
+                    ...movie,
+                    actors: credits.cast,
+                    directors
+                }
+            })
+            console.log(this.state)
+        } catch {
+            this.setState({error: true})
+        } finally {
+            this.setState({loading: false})
+        } 
+        
+    }
+
+    componentDidMount() {
+        this.fetchMovie()
+    }
+
+    render() {
+
+        const { movie, loading, error } = this.state
+
+        if(loading) return <Spinner/>
+        if(error) return <div>Something went wrong...</div>
+        return (
+            <>
+                <BreadCrumb movieTitle={movie.original_title} />
+                <MovieInfo movie={movie} />
+                <MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue} />
+                <Grid header="Actors">
+                    {movie.actors.map(actor => (
+                        <Actor key={actor.credit_id} name={actor.name} character={actor.character} imageUrl={actor.profile_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}` : NoImage} />
+                    ))}
+                </Grid>
+            </>
+        )
+    }
 }
 
-export default Movie;
+const MovieWithParams = props => <Movie {...props} params={useParams()} />
+
+export default MovieWithParams;
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,47 +1,95 @@
-import { useState, useEffect, useRef } from "react";
+// import { useState, useEffect, useRef } from "react";
 import searchIcon from "../../images/search-icon.svg";
 import {Wrapper, Content} from "./SearchBar.styles";
 import PropTypes from "prop-types";
 
-const SearchBar = ({ setSearchTerm }) => {
-    const [state, setState] = useState('')
-    const initial = useRef(true)
+// const SearchBar = ({ setSearchTerm }) => {
+//     const [state, setState] = useState('')
+//     const initial = useRef(true)
 
 
-    useEffect(() => {
-        // 在初始化的時候不希望觸發useEffect
-        // 所以先給個初始值判斷
-        // 等初始化完後再監測
-        // 可以設定console比較差異
-        if(initial.current) {
-            initial.current = false
-            return
+//     useEffect(() => {
+//         // 在初始化的時候不希望觸發useEffect
+//         // 所以先給個初始值判斷
+//         // 等初始化完後再監測
+//         // 可以設定console比較差異
+//         if(initial.current) {
+//             initial.current = false
+//             return
+//         }
+
+//         const timer = setTimeout(() => {
+//             setSearchTerm(state)
+//         }, 500)
+//         // 每次觸發新useEffect rerender之前會先執行return的東西
+//         return () => clearTimeout(timer)
+//         // 雖然有dependency但第一次render仍會觸發
+//     }, [setSearchTerm, state])
+
+//     return (
+//         <Wrapper>
+//         <Content>
+//             <img src={searchIcon} alt="search-icon"/>
+//             <input 
+//             type="text" 
+//             placeholder="Search Movie"
+//             onChange={event => setState(event.target.value)}
+//             value={state}
+//             />
+//         </Content>
+//     </Wrapper>
+//     )
+// }
+
+
+
+// Class Component
+import {Component} from "react";
+
+class SearchBar extends Component {
+    // 無需constructor babel和webopack會轉譯
+    // class元件只有一個狀態
+    state = {value: ''}
+    timeout = null
+
+    // lifecycle
+    componentDidUpdate(_prevProps, prevState) {
+        // 需要this參照
+        if(this.state.value !== prevState.value) {
+            const { setSearchTerm } = this.props
+
+            clearTimeout(this.timeout)
+
+            this.timeout = setTimeout(() => {
+                const { value } = this.state;
+                setSearchTerm(value)
+            }, 500)
         }
+    }
+    
+    render() {
 
-        const timer = setTimeout(() => {
-            setSearchTerm(state)
-        }, 500)
-        // 每次觸發新useEffect rerender之前會先執行return的東西
-        return () => clearTimeout(timer)
-        // 雖然有dependency但第一次render仍會觸發
-    }, [setSearchTerm, state])
+        const {value} = this.state
 
-    return (
-        <Wrapper>
-        <Content>
-            <img src={searchIcon} alt="search-icon"/>
-            <input 
-            type="text" 
-            placeholder="Search Movie"
-            onChange={event => setState(event.target.value)}
-            value={state}
-            />
-        </Content>
-    </Wrapper>
-    )
+        return (
+            <Wrapper>
+            <Content>
+                <img src={searchIcon} alt="search-icon"/>
+                <input 
+                type="text" 
+                placeholder="Search Movie"
+                onChange={event => this.setState({value: event.target.value})}
+                value={value}
+                />
+            </Content>
+            </Wrapper>
+        )
+    }
 }
+
 
 SearchBar.propTypes = {
     callback: PropTypes.func
 }
+
 export default SearchBar;
